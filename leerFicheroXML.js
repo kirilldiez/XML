d61c41ea06d0d02cb7a0b773ee6eded1a8d6c75f
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-14 12:47:14 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,6 +1,6 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
-	let alumnos = xmlDoc.querySelectorAll("libro")
+	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libro.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libro[i].textContent + "</p>"
 	
