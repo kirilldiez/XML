a3afc0464b2e89f92fdeefeb52f7c47bddb66675
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-17 08:50:46 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,4 +1,5 @@
 function gestionarFicheroXML(xmlDoc){
+	alert(xmlDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
