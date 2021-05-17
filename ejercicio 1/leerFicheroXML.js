792168af310bcf1fd72a7f00bf0ff1f6f0c844ed
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-17 08:55:04 +0200

    Add files via upload

---
+++ ejercicio 1/leerFicheroXML.js
@@ -0,0 +1,9 @@
+function gestionarFicheroXML(xmlDoc){
+	alert(xmlDoc)
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+	
+}
+	loadDocA("libros.xml","xml");
