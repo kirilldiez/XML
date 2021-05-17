61176c38139431b70dde076ba2ec7d6c19c91863
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-17 08:54:26 +0200

    Delete leerFicheroXML.js

--- leerFicheroXML.js
+++
@@ -1,9 +0,0 @@
-function gestionarFicheroXML(xmlDoc){
-	alert(xmlDoc)
-	let capaVacia = document.querySelector("#ficheroXML")
-	let libros = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<libros.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
-	
-}
-	loadDocA("libros.xml","xml");
