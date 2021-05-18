5a4e82b910502bd222e2bec16f45ba9b199f82ab
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-18 12:36:16 +0200

    Update leerFicheroXML.js

--- ejercicio2/leerFicheroXML.js
+++ ejercicio2/leerFicheroXML.js
@@ -2,12 +2,12 @@ function gestionarFicheroXML(xmlDoc){
 	
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("msg")
-	for(let i=0; i<conversacion.length; i++)
+	for(let i=0; i<msgs.length; i++)
 		if (i % 2 == 0){
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='color borde impar'>" + conversacion[i].textContent + "</p>";
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='color borde impar'>" + msgs[i].textContent + "</p>";
 		}
 		else{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='borde par'>" + conversacion[i].textContent + "</p>";
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='borde par'>" + msgs[i].textContent + "</p>";
 		}
 		
 }
