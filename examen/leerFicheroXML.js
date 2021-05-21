76e0d93561f9ba87f1155f940d4bbd4af8f85aa4
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-21 09:34:13 +0200

    Update leerFicheroXML.js

--- examen/leerFicheroXML.js
+++ examen/leerFicheroXML.js
@@ -1,7 +1,7 @@
 function gestionarFicheroXML(xmlDoc){
-	alert("new")
+
 	let capaVacia = document.querySelector("#xml")
-	/*let peliculas = xmlDoc.querySelectorAll("pelicula")
+	let peliculas = xmlDoc.querySelectorAll("pelicula")
 	for(let i=0; i<peliculas.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='cine'>" + peliculas[i].textContent + "</p>";
 }
