d359f008a6b38ad1ec673815056f0a6819a1bcb9
kirilldiez <76429582+kirilldiez@users.noreply.github.com>
2021-05-21 09:54:36 +0200

    Delete examen directory

--- examen/leerFicheroXML.js
+++
@@ -1,9 +0,0 @@
-function gestionarFicheroXML(xmlDoc){
-	let capaVacia = document.querySelector("#xml");
-	let pelis = xmlDoc.querySelectorAll("pelicula");
-	for(let i=0; i<pelis.length; i++){
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='cine'>" + pelis[i].textContent + "</p>";
-	}
-}
-	loadDocA("cine.xml","xml");
-	console.log(xmlDoc);
